f95803c3d98e3b39e03f8c8784605de5f9d58dac
Mihir Shetty <ms6556@columbia.edu>
2023-05-17 21:27:32 -0400

    Fixed GenEnergySpectrum.py to only generate energies uniform in log-scale (consistent with sensitivity plots

--- utils/GenEnergySpectrum.py
+++ utils/GenEnergySpectrum.py
@@ -28,6 +28,22 @@ def BrokenPowerLaw(E,**kwargs):
     else:
         return np.power((1/Eref)*E,-Gamma2)
 
+def LogUniform(E,**kwargs):
+## Suppose that you treat this function as a probability distribution.
+## If you draw enough points and set energy to a log scale, then you get a uniform distribution
+## This is desirable for Sensitivity studies since energies span a wide range of orders of magnitude
+    try:
+        a = kwargs["a"]
+    except:
+        print("a not specified")
+        sys.exit()
+    try:
+        b = kwargs["b"]
+    except:
+        print("b not specified")
+        sys.exit()
+    scaling = np.log10(b/a)
+    return np.power(E,-1)/scaling
 
 if __name__ == "__main__":
 ## Read in output file name, min and max energies, and bin width
@@ -35,24 +51,32 @@ if __name__ == "__main__":
                     prog='GenEnergySpectrum.py',
                     description='Generate energy spectrum of background. Log scale on x')
     parser.add_argument('-o', '--OutputFileName', default="EnergySpec.root",help="Name of output file")
-    parser.add_argument('-p','--Plot',action='store_true')
-    parser.add_argument("--minE",type=float, default = -1, help="Minimum energy of range. Note this is log scale (ie 0.1 corresponds to -1)")
-    parser.add_argument("--maxE",type=float, default=1, help="Maximum energy of range.  Note this is log scale (ie 0.1 corresponds to -1)")
+    parser.add_argument('-p','--Plot',action='store_true', help="Plot Energy Spectrum")
+    parser.add_argument("--minE",type=float, default = 0.1, help="Minimum energy of range.")
+    parser.add_argument("--maxE",type=float, default=10, help="Maximum energy of range.")
     parser.add_argument("-n","--NumDiv",default=100, help="Number of divisions")
     args = parser.parse_args()
-
+## Sanity check to make sure that energies are strictly greater than zero and that maxE is greater than minE
+    if((args.minE<=0) or (args.maxE<=0) or (args.maxE<=args.minE)):
+        print("Invalid energies")
+        sys.exit()
+## Create histogram with log binnings
 ## Generate logarithmic binnings
     new_bins = [0]*(args.NumDiv+1)
-    width = (args.maxE-args.minE)/(args.NumDiv)
+    base = np.log10(args.minE)
+    fin = np.log10(args.maxE)
+    width = (fin-base)/(args.NumDiv)
     for i in range(0,args.NumDiv+1):
-        pwr = args.minE+i*width
+        pwr = base+i*width
         new_bins[i] = 10**pwr
-## Create histogram with log binnings
     EnergyHist = ROOT.TH1D("BackgroundSpectrum", 'Background Flux', args.NumDiv, array('d',new_bins) )
-## Get centers of log bins
+## Get centers of bins
     EnergyCenters = [EnergyHist.GetBinCenter(i) for i in range(args.NumDiv)]
 ## Need to modify the following line if you have a different function
-    output = [BrokenPowerLaw(energy,Eref=3,Gamma1=3.3,Gamma2=2) for energy in EnergyCenters]
+# Background MeV Spectra found here: https://iopscience.iop.org/article/10.3847/1538-4357/acab69
+#    output = [BrokenPowerLaw(energy,args.Scale,Eref=3,Gamma1=3.3,Gamma2=2) for energy in EnergyCenters]
+# Standard LogUniform distribution (ie. equal probabilities amoungst log bins)
+    output = [LogUniform(energy,a=args.minE,b=args.maxE) for energy in EnergyCenters]    
 ## Fills output histogram with flux function by filling each bin weighted by the function
     for i in range(len(output)):
         EnergyHist.Fill(EnergyCenters[i],output[i])
