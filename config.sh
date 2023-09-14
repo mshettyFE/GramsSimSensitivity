ccf890013edac0eb8c3cf17df133b501a535e01b
Mihir Shetty <ms6556@columbia.edu>
2023-09-14 11:46:26 -0400

    Added GramsSim submodule and config.sh to build folder for condor jobs

--- config.sh
+++ config.sh
@@ -8,3 +8,16 @@ then
 fi
 cmake ../GramsSim
 make
+rm -rf GramsDetSim
+rm -rf GramsElecSim
+rm -rf GramsG4
+rm -rf GramsReadoutSim
+rm -rf GramsSky
+rm -rf root
+rm -rf scripts
+rm -rf bin
+rm -rf CMakeFiles
+rm -rf utils
+rm -f CMakeCache.txt cmake_install.cmake Makefile
+cp ../ThinFlatGrams.gdml .
+cp ../ThinGrams.gdml .
