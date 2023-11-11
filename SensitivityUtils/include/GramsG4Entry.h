1238ae27b93fe05352ae16558a7b8f065c0d26b6
Mihir Shetty <ms6556@columbia.edu>
2023-11-11 00:46:04 -0500

    Finished GramsG4 class tentatively

--- SensitivityUtils/include/GramsG4Entry.h
+++ SensitivityUtils/include/GramsG4Entry.h
@@ -2,15 +2,27 @@
 #define G4ENTRY
 
 #include "Entry.h"
-#include <any>
 #include <vector>
 
 class GramsG4Entry : public Entry{
   public:
     // Run, Event, TrackID,ParentID,PDGCode,ProcessName,t,x,y,z,Etot,identifier
-    GramsG4Entry(int,int,int,std::string,double,float,float,float,double,int);
+    GramsG4Entry(int, int, int,int,int,std::string,double,float,float,float,double,int);
     // get key
     std::vector<int> extract_key() override;
+
+    int get_run();
+    int get_event();
+    int get_TrackID();
+    int  get_ParentID();
+    int get_PDGCode();
+    std::string get_ProcessName();
+    double get_t();
+    float get_x();
+    float get_y();
+    float get_z();
+    double get_E();
+    int get_ID();
 };
 
 #endif
--- SensitivityUtils/src/GramsG4Entry.cpp
+++ SensitivityUtils/src/GramsG4Entry.cpp
@@ -2,10 +2,10 @@
 #include <tuple>
 
 #include "Entry.h"
-#include "G4Entry.h"
+#include "GramsG4Entry.h"
 
-GramsG4Entry::GramsG4Entry(int run,int event, int TrackID, std::string PDGCode,
-  double t, float x, float y, float z,double Etot,int identifier){
+GramsG4Entry::GramsG4Entry(int run,int event, int TrackID, int ParentID,  int PDGCode, 
+ std::string ProcessName, double t, float x, float y, float z,double Etot,int identifier){
     data.push_back(run);
     data.push_back(event);
     data.push_back(TrackID);
@@ -23,4 +23,49 @@ std::vector<int> GramsG4Entry::extract_key(){
     int event = this->get_value<int>(1);
     std::vector<int> output =  {run, event};
     return output;
+}
+
+int GramsG4Entry::get_run(){
+  return this->get_value<int>(0);
+}
+
+int GramsG4Entry::get_event(){
+    return this->get_value<int>(1);
+}
+
+int GramsG4Entry::get_TrackID(){
+    return this->get_value<int>(2);
+}
+
+int  GramsG4Entry::get_ParentID(){
+    return this->get_value<int>(3);
+}
+
+int GramsG4Entry::get_PDGCode(){
+    return this->get_value<int>(4);
+}
+
+std::string GramsG4Entry::get_ProcessName(){
+    return this->get_value<std::string>(5);
+}
+
+double GramsG4Entry::get_t(){
+    return this->get_value<double>(6);
+}
+
+float GramsG4Entry::get_x(){
+  return this->get_value<float>(7);
+}
+
+float GramsG4Entry::get_y(){
+  return this->get_value<float>(8);
+}
+float GramsG4Entry::get_z(){
+  return this->get_value<float>(9);
+}
+double GramsG4Entry::get_E(){
+  return this->get_value<double>(10);
+}
+int GramsG4Entry::get_ID(){
+  return this->get_value<int>(11);
 }
